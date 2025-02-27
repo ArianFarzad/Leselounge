453a19ff8511539ec20e984503bc7a9db868c5ea
arian <arian.farzad@stud.h-da.de>
2025-02-27 19:43:48 +0100

    add: linter and formatter for backend and frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,26 +3,26 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
 if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI is not defined in the environment variables!");
+  throw new Error('MONGO_URI is not defined in the environment variables!');
 }
 const mongoUri = process.env.MONGO_URI;
 
-mongoose.connect(mongoUri)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(mongoUri)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Test route
 app.get('/', (req, res) => {
-    res.send('Backend is running!');
+  res.send('Backend is running!');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
